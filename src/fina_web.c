a3d17218e5725c783ecee70e883c9ecba5395714
Leevi Uotinen <uotinen.leevi@hotmail.com>
2019-01-30 19:56:29 +0200

    Update to use new libuo APIs

--- src/fina_web.c
+++ src/fina_web.c
@@ -2,15 +2,16 @@
 #include "uo_http.h"
 #include "uo_http_server.h"
 #include "uo_err.h"
+#include "uo_prog.h"
 
 #include <stdio.h>
 
 void http_server_before_send_response(
     uo_cb *cb)
 {
-    uo_http_conn *http_conn = uo_cb_stack_index(cb, 2);
+    uo_http_sess *http_sess = uo_cb_stack_index(cb, 0);
 
-    uo_http_response_set_header(http_conn->http_response, "server", "libuo http");
+    uo_http_msg_set_header(http_sess->http_response, "server", "libuo http");
     uo_cb_invoke(cb);
 }
 
@@ -27,15 +28,15 @@ int main(
 
     uo_http_server *http_server = uo_http_server_create(port);
 
-    uo_cb_append(http_server->evt_handlers.before_send_response, http_server_before_send_response);
+    uo_cb_append(http_server->evt_handlers.before_send_msg, http_server_before_send_response);
 
     if (!uo_http_server_set_opt_serve_static_files(http_server, root_dir))
         uo_err_exit("Error while setting root directory.");
 
     uo_http_server_start(http_server);
 
-    printf("Press 'q' to quit...\n");
-    while(getchar() != 'q');
+    uo_prog_init();
+    uo_prog_wait_for_sigint();
 
     uo_http_server_destroy(http_server);
     uo_conf_destroy(conf);
